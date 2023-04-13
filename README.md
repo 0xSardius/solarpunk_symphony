d7581e5b192a0f2ba3bca45c4a4ccc01fedef5c5
Sardius <0xsardius@gmail.com>
2023-04-13 00:44:56 -0400

    Got basic design down

--- README.md
+++ README.md
@@ -1,35 +1,3 @@
-## Getting Started
+## Solarpunk Symphony
 
-Create a project using this example:
-
-```bash
-npx thirdweb create --template next-typescript-starter
-```
-
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-On `pages/_app.tsx`, you'll find our `ThirdwebProvider` wrapping your app, this is necessary for our [hooks](https://portal.thirdweb.com/react) and
-[UI Components](https://portal.thirdweb.com/ui-components) to work.
-
-### Deploy to IPFS
-
-Deploy a copy of your application to IPFS using the following command:
-
-```bash
-yarn deploy
-```
-
-## Learn More
-
-To learn more about thirdweb and Next.js, take a look at the following resources:
-
-- [thirdweb React Documentation](https://docs.thirdweb.com/react) - learn about our React SDK.
-- [thirdweb TypeScript Documentation](https://docs.thirdweb.com/typescript) - learn about our JavaScript/TypeScript SDK.
-- [thirdweb Portal](https://docs.thirdweb.com) - check our guides and development resources.
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-
-You can check out [the thirdweb GitHub organization](https://github.com/thirdweb-dev) - your feedback and contributions are welcome!
-
-## Join our Discord!
-
-For any questions, suggestions, join our discord at [https://discord.gg/thirdweb](https://discord.gg/thirdweb).
+Solarpunk Symphony" is a captivating collection of seven NFTs that embody the essence of solarpunk—a vision of a sustainable and harmonious future where technology and nature coexist. Each piece in this collection offers a glimpse into a world where renewable energy, ecological balance, and community-driven innovation thrive, inspiring hope and a collective sense of responsibility towards our planet.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
-import { ConnectWallet, useContract, useMetadata } from "@thirdweb-dev/react";
+import { ConnectWallet, MediaRenderer, useContract, useMetadata, useContractRead } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 // import styles from "../styles/Home.module.css";
-import { Container, Flex, Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { Container, Flex, Box, SimpleGrid, Heading, Skeleton, Text } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   const contractAddress = "0xbC1dE8684e47aBc14eeD008ec1f9C54b600bCec7";
   const { contract } = useContract(contractAddress);
   const {data: metadata, isLoading: isLoadingMetadata} = useMetadata(contract);
+  const {data: totalMinted, isLoading: isLoadingTotalMinted} = useContractRead(contract, "totalMinted");
+
   return (
     <>
     <Container>
@@ -15,10 +17,19 @@ const Home: NextPage = () => {
         <ConnectWallet />
       </Flex>
       <Flex h={"90vh"} alignItems={"center"} justifyContent={"center"}>
-        <SimpleGrid columns={2} spacing={10}>
-          <Box></Box>
-          <Flex>
-            <Text>NFT Project</Text>
+        <SimpleGrid columns={2} spacing={10} justifyItems={"center"}>
+          <Box>
+            <Skeleton isLoaded={!isLoadingMetadata}>
+              <MediaRenderer src={(metadata as {image: string})?.image} />
+            </Skeleton>
+          </Box>
+          <Flex direction={"column"}>
+            <Skeleton isLoaded={!isLoadingMetadata}>
+              <Heading>{(metadata as {name?: string})?.name}</Heading>
+              </Skeleton>
+            <Skeleton isLoaded={!isLoadingMetadata}>
+              <Text>{(metadata as {description?: string})?.description}</Text>
+              </Skeleton>
           </Flex>
           </SimpleGrid>
       </Flex>
